d2c22ea520b5923ad2adca046c92ab8495cc014f
CEN333-Spring23-24 <CEN333-Spring23-24@outlook.com>
2024-07-18 12:04:43 +0300

    With sample problems

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,16 +1,17 @@
-import { Component,EventEmitter } from '@angular/core';
+import { Component } from '@angular/core';
 import { MContainerComponent } from '../../m-framework/m-container/m-container.component';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { Car } from '../../data/CarData';
-import { MCardComponent } from '../../m-framework/m-card/m-card.component';
+import { Car } from '../../data/Car';
 import { MTableComponent } from "../../m-framework/m-table/m-table.component";
 import { MSearchButtonComponent } from "../../m-framework/m-search-button/m-search-button.component";
+import { MLocaldataService } from '../../services/m-localdata.service';
+import { CanvasJSAngularChartsModule } from '@canvasjs/angular-charts';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, FormsModule, MContainerComponent, MCardComponent, MTableComponent, MSearchButtonComponent],
+  imports: [CanvasJSAngularChartsModule,CommonModule, FormsModule, MContainerComponent, MTableComponent, MSearchButtonComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
@@ -19,34 +20,107 @@ export class HomeComponent{
   make: string; 
   year: number; 
   filterTerm: string = "";
-  listofcars: Car[]; 
   headers: string[] = ['ID','Make','Model','Year'];
+  
+  // This is code added for live charts from CanvasJS example
+  dataPoints:any[] = [];
+  chart: any;
+  chartOptions = {
+    theme: "light2",
+    title: {
+      text: "Live Data"
+    },
+    data: [{
+      type: "bar",
+      dataPoints: this.dataPoints
+    }]
+  }
+  // Added code ends here 
 
-  id: number = 1;
-
-  constructor(){
+  constructor(private mLocaldataService:MLocaldataService){
     this.model = "";
     this.make = "";
     this.year = 2019;
-    this.listofcars = []; 
     
   }
+  getChartInstance(chart: object) {
+    this.chart = chart;
+    //this.updateData();
+  }
+  updateData(){
+    let myMap = this.applicationLogic(); 
+    this.dataPoints.splice(0, this.dataPoints.length);
+    for (let [key,value] of myMap)      
+        this.dataPoints.push({x: key, y: value});
+    this.chart.render();
+  }
+  applicationLogic(){ // this kind of code is what I am looking for in a project
+    
+    //Histogram Logic 
 
+    // Sample Question 1
+
+    // People register for one of three trips (to Dubai, to AlAin, and to Sharjah)
+    // They provide their phone number and their name. 
+    // A bar chart shows the number of people on each trip. 
+    // A table also shows who is going to each trip in a first come first served way
+    // There is a limit of 50 people in the bus. If the limit is reached
+    // No more registrations to be accepted to this trip. 
+    // A box shows saying this trip is complete. 
+
+    // Sample Question 2
+
+    // How many times does a patient fever reaches the high range (40 and above)
+    // elevated (38 to 39.9), and normal (below 38). Readings need to be
+    // one hour apart to count. 
+    // If the reading is high, a box appears asking the patient 
+    // to seek help. If the temperature is elevated for more than 24
+    // hours, again the same box appears providing the reason as
+    // consistently elevated temp. 
+
+    let myMap = new Map<string, number>(); 
+    this.mLocaldataService.getList().forEach(car => {
+        myMap.set(car.year,0);
+    });
+    let years = myMap.keys();
+    for(let year of years){
+      let count = 0; 
+      this.mLocaldataService.getList().forEach(car =>{
+        if(car.year == year)
+          count++; 
+      });
+      myMap.set(year,count);
+    } 
+    return myMap;
+  }
+  public get listofcars(){
+    return this.mLocaldataService.getList(); 
+  }
   addCar(){
-    let car = new Car(this.id++,this.make,this.model,this.year);
-    this.listofcars = [...this.listofcars, car];
+    
+    let car = new Car(
+      this.mLocaldataService.getNextID(),
+      this.make,
+      this.model,
+      this.year
+    );
+    this.mLocaldataService.add(car);
+    this.updateData();
     this.make = "";
     this.model = "";
     this.year = 2019;
+
   }
+
   removeCar(carID: number){
-    console.log("Request to remove CARID " + carID);
-    let index = this.listofcars.findIndex(anycar => anycar.id == carID);
-    if(index != -1) 
-      this.listofcars.splice(index,1);
+    this.mLocaldataService.remove(carID);
   }
   removeAll(){
-    this.listofcars = [];
+    this.mLocaldataService.removeAll();
   }
+  navigateCar(){
+    console.log("Called");
+  }
+ 
 }
 
